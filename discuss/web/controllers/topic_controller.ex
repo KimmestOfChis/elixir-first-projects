ba93405ce918efa30f887921267e18b78cdd554d
EmjayFlight <memjay3279@gmail.com>
2017-07-30 23:24:17 -0400

    Simple CRUD Added

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -5,7 +5,12 @@ defmodule Discuss.TopicController do
   alias Discuss.Topic
 
   use Discuss.Web, :controller
-    def new(conn, params) do
+    def index(conn, _params) do
+      topics = Repo.all(Topic)
+      render conn, "index.html", topics: topics
+    end
+
+    def new(conn, _params) do
       struct = %Topic{} #aliased Discuss.Topic -> Topic
       params = %{}
       changeset = Topic.changeset(%Topic{}, %{})
@@ -14,6 +19,44 @@ defmodule Discuss.TopicController do
     end
 
     def create(conn, %{"topic" => topic} ) do
-      
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+      end
     end
+
+    def edit(conn, %{"id" => topic_id}) do
+      topic = Repo.get(Topic, topic_id)
+      changeset = Topic.changeset(topic)
+
+      render conn, "edit.html", changeset: changeset, topic: topic
+    end
+
+    def update(conn, %{"id" => topic_id, "topic" => topic}) do
+      old_topic = Repo.get(Topic, topic_id)
+      changeset = Topic.changeset(old_topic, topic)
+
+      case Repo.update(changeset) do
+        {:ok, _topic} ->
+          conn
+          |> put_flash(:info, "Topic Updated")
+          |> redirect(to: topic_path(conn, :index))
+        {:error, changeset} ->
+          render conn, "edit.html", changeset: changeset, topic: old_topic
+      end
+    end
+
+    def delete(conn, %{"id" => topic_id}) do
+      Repo.get!(Topic, topic_id) |> Repo.delete!
+
+      conn
+      |> put_flash(:info, "Topic Successfully Deleted")
+      |> redirect(to: topic_path(conn, :index))
+    end
+
 end
--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -16,11 +16,14 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    #whenever somone sends my site a GET request with topics/new
+    #get "/", TopicController, :index
+    #whenever somone sends my site a GET request with "topics/new"
     #Enact the topic controller's "new" function
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
+    #get "/topics/new", TopicController, :new
+    #post "/topics", TopicController, :create
+    #get "topics/:id/edit", TopicController, :edit
+    #put "/topics/:id", TopicController, :update
+    resources "/", TopicController #only works if following restful conventions
   end
 
   # Other scopes may use custom stacks.
